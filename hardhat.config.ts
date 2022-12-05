d3935f5c08052d597471f17e972e8c6a7139bb61
JakubKub13 <jakubkubala3@gmail.com>
2022-12-05 13:01:03 +0100

    Debuggin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,60 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "privatKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || ""
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
+const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || ""
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: POLYGON_MAINNET_RPC_URL
+      },
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
+  solidity: {
+    compilers: [
+      {version: "0.8.17"},
+    ],
+  },
+  etherscan: {
+    //apiKey: ETHERSCAN_API_KEY,
+    apiKey: POLYGONSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
 };
 
 export default config;
--- test/ConstantProductAMM.test.ts
+++ test/ConstantProductAMM.test.ts
@@ -1,24 +1,70 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { ethers } from "hardhat"
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers, network } from "hardhat"
 import { ConstantProductAMM, ConstantProductAMM__factory } from "../typechain-types";
+
+const DAI_WHALE: string = "0x075e72a5eDf65F0A5f44699c7654C1a76941Ddc8";
+const USDC_WHALE: string = "0xB60C61DBb7456f024f9338c739B02Be68e3F545C";
+
+const TOKEN_A_ADDRESS: string = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"; // DAI polygon
+const TOKEN_B_ADDRESS: string = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // USDC polygon
+const AMOUNT_DAI_TO_SEND = 1000n * 10n ** 18n // 1000 DAI
+const AMOUNT_USDC_TO_SEND = 1000n * 10n ** 18n // 1000 USDC
+
 
 describe("ConstantProductAMM", () => {
     let owner: SignerWithAddress;
     let acc1: SignerWithAddress;
     let acc2: SignerWithAddress;
+    let daiWhale: SignerWithAddress;
+    let usdcWhale: SignerWithAddress;
     let cAmmFactory: ConstantProductAMM__factory;
     let constantProductAMM: ConstantProductAMM;
-    const tokenAaddr: string = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"; // DAI polygon
-    const tokenBaddr: string = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // USDC polygon
-    
+    let dai: Contract;
+    let usdc: Contract;
+
 
     beforeEach(async () => {
         [owner, acc1, acc2] = await ethers.getSigners();
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [DAI_WHALE]
+        });
+
+        dai = await ethers.getContractAt("IERC20", TOKEN_A_ADDRESS);
+        daiWhale = await ethers.getSigner(DAI_WHALE);
+        await dai.connect(daiWhale).transfer(owner.address, AMOUNT_DAI_TO_SEND);
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [USDC_WHALE]
+        });
+
+        usdc = await ethers.getContractAt("IERC20", TOKEN_B_ADDRESS);
+        usdcWhale = await ethers.getSigner(USDC_WHALE);
+        await usdc.connect(usdcWhale).transfer(owner.address, AMOUNT_USDC_TO_SEND);
+
         cAmmFactory = await ethers.getContractFactory("ConstantProductAMM");
         constantProductAMM = await cAmmFactory.deploy(
-            tokenAaddr,
-            tokenBaddr
+            TOKEN_A_ADDRESS,
+            TOKEN_B_ADDRESS
         );
         await constantProductAMM.deployed();
+    })
+
+    it("Should deploy without errors", async () => {
+        let _tokenA = await constantProductAMM.tokenA();
+        let _tokenB = await constantProductAMM.tokenB();
+        expect(_tokenA).to.eq("0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063");
+        expect(_tokenB).to.eq("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
+    });
+
+    it("Owner account should be funded on forked mainnet with DAI and USDC", async () => {
+        const daiOwnerBal = await dai.balanceOf(owner.address);
+        const usdcOwnerBal = await usdc.balanceOf(owner.address);
+        console.log(`Dai balance of owner is: ${ethers.utils.formatEther(daiOwnerBal)}`);
+        console.log(`USDC balance of owner is: ${ethers.utils.formatEther(usdcOwnerBal)}`);
     })
 })
