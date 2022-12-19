e471b120584028c34dc18db9e4e99af4c503213b
JakubKub13 <jakubkubala3@gmail.com>
2022-12-19 10:25:42 +0100

    Working on createPair function

--- contracts/DEXFactory.sol
+++ contracts/DEXFactory.sol
@@ -6,18 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract DEXFactory is Ownable {
-    mapping(address => bool) public isPairCreated;
+    mapping(address => mapping(address => bool)) public isPairCreated;
     uint256 public ownerFeePool;
     uint256 public immutable createPairFee;
 
     event PairCreated(address tokenA, address tokenB, address createdPair);
-    event FeeWthdrawal(address to, uint256 amount, unt256 time);
+    event FeeWthdrawal(address to, uint256 amount, uint256 time);
 
     constructor(uint256 _creationFee) {
         createPairFee = _creationFee;
     }
 
-    function
+    function createPair(address tokenA, address tokenB) external {
+        require(isPairCreated[tokenA][tokenB] == false && isPairCreated[tokenB][tokenA] == false, "DEXFactory: This tokens already has pair created");
+        
+    }
 
 
 
