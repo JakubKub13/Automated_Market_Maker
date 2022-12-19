3b5f0d2c9200496da42a8c41e80feb05bda31a46
JakubKub13 <jakubkubala3@gmail.com>
2022-12-19 10:16:45 +0100

    createPair function

--- contracts/DEXFactory.sol
+++ contracts/DEXFactory.sol
@@ -10,7 +10,15 @@ contract DEXFactory is Ownable {
     uint256 public ownerFeePool;
     uint256 public immutable createPairFee;
 
-    event PairCreated()
+    event PairCreated(address tokenA, address tokenB, address createdPair);
+    event FeeWthdrawal(address to, uint256 amount, unt256 time);
+
+    constructor(uint256 _creationFee) {
+        createPairFee = _creationFee;
+    }
+
+    function
+
 
 
 
