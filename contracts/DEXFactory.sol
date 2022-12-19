dd5eb40cafb789dcc8d229baca0dcea78e44b565
JakubKub13 <jakubkubala3@gmail.com>
2022-12-19 10:08:00 +0100

    Working on pair factory

--- contracts/DEXFactory.sol
+++ contracts/DEXFactory.sol
@@ -2,11 +2,17 @@
 pragma solidity 0.8.17;
 
 import "./ConstantProductAMM.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract DEXFactory is AccessControl {
-    address public owner;
-    ConstantProductAMM public pair;
+contract DEXFactory is Ownable {
+    mapping(address => bool) public isPairCreated;
+    uint256 public ownerFeePool;
+    uint256 public immutable createPairFee;
+
+    event PairCreated()
+
+
+
 
 }
